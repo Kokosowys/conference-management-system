a91f5e4a7e06dac94b6b4e7d3281f1f070bd410f
Rafał Sztandera <rafszt@gmail.com>
2017-05-28 23:09:23 +0200

    requirements update; short pass

--- backend/api.py
+++ backend/api.py
@@ -9,7 +9,7 @@ from itsdangerous import (TimedJSONWebSignatureSerializer
 
 # initialization
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
+app.config['SECRET_KEY'] = '!haselko'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
