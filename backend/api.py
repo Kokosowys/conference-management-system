c3177f88c75ecabaece8e05bf0fb19519dcd0249
Rafał Sztandera <rafszt@gmail.com>
2017-06-04 19:29:50 +0200

    add article and attachment models

--- backend/api.py
+++ backend/api.py
@@ -6,6 +6,7 @@ from functools import partial
 
 from flask import Flask, current_app, abort, request, jsonify, g, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 from flask_httpauth import HTTPBasicAuth
 from passlib.apps import custom_app_context as pwd_context
 from itsdangerous import (TimedJSONWebSignatureSerializer
@@ -14,6 +15,8 @@ from flask_principal import (AnonymousIdentity, Identity,
     identity_changed, identity_loaded, Permission, Principal,
     RoleNeed, ActionNeed, UserNeed)
 
+# contraints
+ATTACHMENTS_DIR = '/home/rsztandera/conference-management-system/attachments'
 
 # initialization
 app = Flask(__name__)
@@ -27,19 +30,8 @@ db = SQLAlchemy(app)
 auth = HTTPBasicAuth()
 principals = Principal(app, skip_static=True)
 
-# Needs
-articleNeed = namedtuple('article', ['method', 'value'])
-editArticleNeed = partial(articleNeed, 'edit')
-apps_needs = [articleNeed, editArticleNeed]
-
-# Permissions
-class EditArticlePermission(Permission):
-    def __init__(self, article_id):
-        need = EditArticleNeed(unicode(article_id))
-        super(EditArticlePermission, self).__init__(need)
-
-#Model classes (DAO)
-class User(db.Model):
+# Model classes (DAO)
+class Person(db.Model):
     __tablename__ = 'person'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), index=True)
@@ -87,42 +79,85 @@ class User(db.Model):
             return None    # valid token, but expired
         except BadSignature:
             return None    # invalid token
-        user = User.query.get(data['id'])
-        return user
+        person = Person.query.get(data['id'])
+        return person
 
 
-@auth.verify_password
-def verify_password(username_or_token, password):
-    # first try to authenticate by token
-    user = User.verify_auth_token(username_or_token)
-    if not user:
-        # try to authenticate with name/password
-        user = User.query.filter_by(name=username_or_token).first()
-        if not user or not user.verify_password(password):
-            return False
-    g.user = user
-    identity_changed.send(current_app._get_current_object(),
-        identity=Identity(user.id))
-    return True
+class Article(db.Model):
+    __tablename__ = 'article'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), index=True)
+    theme = db.Column(db.String(32))
+    label = db.Column(db.String(32))
+    description = db.Column(db.String(500))
+    text = db.Column(db.String(1024))
+    personId = db.Column(
+        db.Integer,
+        db.ForeignKey('person.id', ondelete='CASCADE'),
+        nullable=False,
+    )
+    person = relationship('Person', backref='articles')
+    # attchments are assigned by articleId in attchment obj
 
+class Attachment(db.Model):
+    __tablename__ = 'attachment'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), index=True)
+    articleId = db.Column(
+        db.Integer,
+        db.ForeignKey('article.id', ondelete='CASCADE'),
+        nullable=False,
+    )
+    article = relationship('Article', backref='attachments')
+
+
+# Roles security setup - needs
+articleNeed = namedtuple('article', ['method', 'value'])
+editArticleNeed = partial(articleNeed, 'edit')
+apps_needs = [articleNeed, editArticleNeed]
+
+# Roles security setup - Permissions
+class EditArticlePermission(Permission):
+    def __init__(self, article_id):
+        need = EditArticleNeed(unicode(article_id))
+        super(EditArticlePermission, self).__init__(need)
+
+# Roles security setup - methods
 @identity_loaded.connect_via(app)
 def on_identity_loaded(sender, identity):
     print 'on_identity_loaded: identity: {}'.format(identity)
-    user = g.user
+    person = g.person
 
-    if hasattr(user, 'id'):
-        identity.provides.add(UserNeed(user.id))
+    if hasattr(person, 'id'):
+        identity.provides.add(UserNeed(person.id))
 
-    if hasattr(user, 'roles'):
-        for role in user.get_roles():
+    if hasattr(person, 'roles'):
+        for role in person.get_roles():
             identity.provides.add(RoleNeed(role))
 
-    # if hasattr(user, 'posts'):
-    #     for post in user.posts:
+    # if hasattr(person, 'posts'):
+    #     for post in person.posts:
     #         identity.provides.add(EditArticleNeed(unicode(post.id)))
 
-    identity.user = user
+    identity.person = person
 
+# Authentication
+@auth.verify_password
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    person = Person.verify_auth_token(username_or_token)
+    if not person:
+        # try to authenticate with name/password
+        person = Person.query.filter_by(name=username_or_token).first()
+        if not person or not person.verify_password(password):
+            return False
+    g.person = person
+    identity_changed.send(current_app._get_current_object(),
+        identity=Identity(person.id))
+    return True
+
+
+# REST API
 @app.route('/api/users', methods=['POST'])
 def new_user():
     name = request.json.get('name')
@@ -133,37 +168,38 @@ def new_user():
     academicDegree = request.json.get('academicDegree')
     if name is None or password is None:
         abort(400)    # missing arguments
-    if User.query.filter_by(name=name).first() is not None:
-        abort(400)    # existing user
-    user = User(name=name, surname=surname, sex=sex, age=age,
+    if Person.query.filter_by(name=name).first() is not None:
+        abort(400)    # existing person
+    person = Person(name=name, surname=surname, sex=sex, age=age,
         academicDegree=academicDegree)
-    user.hash_password(password)
-    db.session.add(user)
+    person.hash_password(password)
+    db.session.add(person)
     db.session.commit()
-    return (jsonify({'name': user.name}), 201,
-            {'Location': url_for('get_user', id=user.id, _external=True)})
+    return (jsonify({'name': person.name}), 201,
+            {'Location': url_for('get_user', id=person.id, _external=True)})
 
 
 @app.route('/api/users/<int:id>')
 def get_user(id):
-    user = User.query.get(id)
-    if not user:
+    person = Person.query.get(id)
+    if not person:
         abort(400)
-    return jsonify({'name': user.name})
+    return jsonify({'name': person.name})
 
 
 @app.route('/api/token')
 @auth.login_required
 def get_auth_token():
-    token = g.user.generate_auth_token(600)
+    token = g.person.generate_auth_token(600)
     return jsonify({'token': token.decode('ascii'), 'duration': 600})
 
 
 @app.route('/api/resource')
 @auth.login_required
 def get_resource():
-    return jsonify({'data': 'Hello, %s!' % g.user.name})
+    return jsonify({'data': 'Hello, %s!' % g.person.name})
 
+# Server invoke
 if __name__ == '__main__':
     if not os.path.exists('db.sqlite'):
         db.create_all()
